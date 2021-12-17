ec744607e669fdf346bdaa9c03e5a1cb524c7411
eduardonk9999 <thiago.novak7@gmail.com>
2021-12-17 17:19:00 -0300

    Add, header, footer

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 <!DOCTYPE html>
 <html>
   <?php include('templates/header.php'); ?>
+  <?php include('templates/footer.php'); ?>
 
 </body>
 </html>
--- templates/header.php
+++ templates/header.php
@@ -1,6 +1,14 @@
 <head>
   <title>Pizzas</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+  <style>
+    .brand{
+      background: #cbb09c !important;
+    }
+    .brand-text{
+      color: #cbb08c !important; 
+    }
+  </style>
 </head>
 <body class="grey lighten-4">
   <nav class="white z-depth-0">
