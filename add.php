956dcba9975a14cbf6d5dcf13b2b92a4d69b44b3
eduardonk9999 <thiago.novak7@gmail.com>
2021-12-20 11:09:32 -0300

    Add, end validation

--- add.php
+++ add.php
@@ -1,10 +1,10 @@
 <?php
-
+  $title = $email= $ingredientes = '';
   $erros = array('email'=>'', 'title'=>'', 'ingredientes'=>'');
 
   if(isset($_POST['submit'])) {
     if(empty($_POST['email'])){
-      echo 'Ah email is required <br />';
+      $erros['email'] = 'Ah email is required <br />';
     } else {
       $email = $_POST['email'];
       if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
@@ -12,7 +12,7 @@
       }
     }
     if(empty($_POST['email'])){
-      echo 'A title is required <br />';
+      $erros['title'] = 'A title is required <br />';
     } else {
       $title = $_POST['title'];
       if(!preg_match('/^[a-zA-Z\s]+$/', $title)) {
@@ -20,7 +20,7 @@
       }
     }
     if(empty($_POST['ingredientes'])){
-      echo 'At least one ingredient is required <br />';
+      $erros['ingredientes'] = 'At least one ingredient is required <br />';
     } else {
       $ingredientes = $_POST['ingredientes'];
       if(!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredientes)) {
@@ -37,12 +37,12 @@
     <h4 class="center">Add a Pizza</h4>
     <form class="white" action="add.php" method="POST">
       <label>Your Email:</label>
-      <input type="text" name="email">
+      <input type="text" name="email" value="<?php echo htmlspecialchars($email); ?>">
       <div class="red-text"><?php echo $erros['email']; ?></div>
       <label>Pizza Title:</label>
-      <input type="text" name="title">
+      <input type="text" name="title" value="<?php echo htmlspecialchars($title); ?>">
       <label>Ingredients (comma separated):</label>
-      <input type="text" name="ingredientes">
+      <input type="text" name="ingredientes" value="<?php echo htmlspecialchars($ingredientes); ?>">
       <div class="center">
         <input type="submit" name="submit" value="submit" class="btn brand z-depth-0">
       </div>
